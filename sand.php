79a55eb633b7b67f99efbbdd53095f1f98f19096
Kamil <a.kamilzan@gmail.com>
2021-02-04 23:27:35 +0300

    form

--- sand.php
+++ sand.php
@@ -2,27 +2,35 @@
 $fio = $_POST['fio'];
 $email = $_POST['email'];
 $date = $_POST['date'];
+$tel = $_POST['tel'];
+
 //Создаем переменные
 
 $fio = htmlspecialchars($fio);
 $email = htmlspecialchars($email);
 $date =htmlspecialchars($date);
+$tel =htmlspecialchars($tel);
 //фильтр преобразования символов
 
 $fio = urldecode($fio);
 $email = urldecode($email);
 $date = urldecode($date);
+$tel = urldecode($tel);
 //Декодирование Урла
 
 $fio = trim($fio);
 $email = trim($email);
 $date = trim($date);
+$tel = trim($tel);
 //Удаление пробелов с конца и начала формы
 
 
-mail("yararkzn@yandex.ru", "Заявка с сайта", "ФИО:".$fio.". Date: ".$date ,"From:yararkzn@yandex.ru \r\n");
+mail("yararkzn@yandex.ru", "Заявка с сайта", "ФИО:".$fio.". Date: ".$date.". Tel:" .$tel,"From: yararkzn@yandex.ru \r\n");
 
-if (mail("yararkzn@yandex.ru", "Заявка с сайта", "ФИО:".$fio.". Date: ".$date ,"From: yararkzn@yandex.ru \r\n"))
+
+
+
+if (mail("yararkzn@yandex.ru", "Заявка с сайта", "ФИО:".$fio.". Date: ".$date.". Tel:" .$tel,"From: yararkzn@yandex.ru \r\n"))
  {
     header('location: thank-you.html');
 } else {
